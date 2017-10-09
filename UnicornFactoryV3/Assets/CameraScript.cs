b6b06e94608b5ba3e6bc92d5d2939d3f36f777e1
Tehpai <32651064+Tehpai@users.noreply.github.com>
2017-10-09 20:16:03 +0100

    Proj v1
    projeto com as cenas que fiz ass veríssimo

---
+++ UnicornFactoryV3/Assets/CameraScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    GameObject player;
+    Transform playerTrans;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        player = GameObject.Find("Character");
+        playerTrans = player.GetComponent<Transform>();
+        transform.position = new Vector3(playerTrans.position.x, playerTrans.position.y, transform.position.z);
+
+
+
+    }
+}
